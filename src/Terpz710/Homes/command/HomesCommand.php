011182b9af2434ed48d20b8c054375e837a4deba
Blueberry627 <145240944+Blueberry627@users.noreply.github.com>
2023-09-17 03:34:51 -0700

    Update HomesCommand.php

--- src/Terpz710/Homes/command/HomesCommand.php
+++ src/Terpz710/Homes/command/HomesCommand.php
@@ -7,37 +7,44 @@ namespace Terpz710\Homes\Command;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-use Terpz710\Homes;
+use pocketmine\utils\Config;
+use Terpz710\Homes\Main;
 
 class HomesCommand extends Command {
 
-    public function __construct() {
+    private $dataFolder;
+
+    public function __construct(string $dataFolder) {
         parent::__construct("homes", "List your available home locations");
         $this->setPermission("homes.homes");
+        $this->dataFolder = $dataFolder;
     }
 
     public function execute(CommandSender $sender, string $label, array $args): bool {
         if ($sender instanceof Player) {
-            $player = $sender;
-
             if (!$this->testPermission($sender)) {
                 $sender->sendMessage("You do not have permission to use this command.");
                 return true;
             }
 
-            $homes = $this->homeManager->getHomes($player);
+            $homes = $this->loadHomeData($sender);
 
             if (empty($homes)) {
                 $sender->sendMessage("You have not set any home locations.");
             } else {
-                $sender->sendMessage("Your home locations:");
-                foreach ($homes as $homeName => $homeLocation) {
-                    $sender->sendMessage("- $homeName: X=" . $homeLocation['x'] . ", Y=" . $homeLocation['y'] . ", Z=" . $homeLocation['z'] . ", World=" . $homeLocation['world']);
-                }
+                $homesList = implode(", ", array_keys($homes));
+                $sender->sendMessage("Homes: " . $homesList);
             }
         } else {
             $sender->sendMessage("This command can only be used in-game.");
         }
         return true;
+    }
+
+    private function loadHomeData(Player $player): array {
+        $playerName = strtolower($player->getName());
+        $config = new Config($this->dataFolder . "homes/" . $playerName . ".yml", Config::YAML);
+
+        return $config->get("homes", []);
     }
 }
