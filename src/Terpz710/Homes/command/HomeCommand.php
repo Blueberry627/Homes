4920ab2bfd044d69f8919b01048ada70595cb6fa
Blueberry627 <145240944+Blueberry627@users.noreply.github.com>
2023-09-17 03:33:05 -0700

    Update HomeCommand.php

--- src/Terpz710/Homes/command/HomeCommand.php
+++ src/Terpz710/Homes/command/HomeCommand.php
@@ -7,26 +7,35 @@ namespace Terpz710\Homes\Command;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
+use pocketmine\utils\Config;
 use pocketmine\math\Vector3;
-use Terpz710\Homes;
+use Terpz710\Homes\Main;
 
 class HomeCommand extends Command {
 
-    public function __construct() {
+    private $dataFolder;
+
+    public function __construct(string $dataFolder) {
         parent::__construct("home", "Teleport to your home location");
         $this->setPermission("homes.home");
+        $this->dataFolder = $dataFolder;
     }
 
     public function execute(CommandSender $sender, string $label, array $args): bool {
         if ($sender instanceof Player) {
-            $player = $sender;
-
             if (!$this->testPermission($sender)) {
                 $sender->sendMessage("You do not have permission to use this command.");
                 return true;
             }
 
-            $homeLocation = $this->homeManager->getHome($player);
+            if (empty($args)) {
+                $sender->sendMessage("Usage: /home <home>");
+                return false;
+            }
+
+            $homeName = $args[0];
+
+            $homeLocation = $this->loadHomeData($sender, $homeName);
 
             if ($homeLocation !== null) {
                 $x = $homeLocation['x'];
@@ -34,24 +43,22 @@ class HomeCommand extends Command {
                 $z = $homeLocation['z'];
                 $worldName = $homeLocation['world'];
 
-                $world = $this->homeManager->getServer()->getLevelByName($worldName);
+                $world = $sender->getServer()->getLevelByName($worldName);
 
                 if ($world !== null) {
-             
                     $homeVector = new Vector3($x, $y, $z);
-
-                    $player->teleport($homeVector, $world);
-
-                    $sender->sendMessage("Teleported to your home location.");
+                    $sender->teleport($homeVector, $world);
+                    $sender->sendMessage("Teleported to your home location '$homeName'.");
                 } else {
                     $sender->sendMessage("The world of your home location no longer exists.");
                 }
             } else {
-                $sender->sendMessage("You have not set a home location.");
+                $sender->sendMessage("You do not have a home location named '$homeName'.");
             }
         } else {
             $sender->sendMessage("This command can only be used in-game.");
         }
         return true;
     }
-}
+
+    private function
