0e70d3655f07598067b3137bacea544d5ddeb7b7
Blueberry627 <145240944+Blueberry627@users.noreply.github.com>
2023-09-17 03:32:02 -0700

    Update DelHomeCommand.php

--- src/Terpz710/Homes/command/DelHomeCommand.php
+++ src/Terpz710/Homes/command/DelHomeCommand.php
@@ -8,7 +8,7 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
 use pocketmine\utils\Config;
-use Terpz710\Main;
+use Terpz710\homes\Main;
 
 class DelHomeCommand extends Command {
 
