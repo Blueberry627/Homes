98ce6f6a86c7d09ff41a635ac2f2f11cb5485181
Blueberry627 <145240944+Blueberry627@users.noreply.github.com>
2023-09-17 03:29:57 -0700

    Update Main.php

--- src/Terpz710/Homes/Main.php
+++ src/Terpz710/Homes/Main.php
@@ -5,22 +5,28 @@ declare(strict_types=1);
 namespace Terpz710\Homes;
 
 use pocketmine\plugin\PluginBase;
-use Terpz710\Homes\Command\HomeManager;
+use pocketmine\utils\Config;
+use Terpz710\Homes\Command\SetHomeCommand;
 use Terpz710\Homes\Command\HomeCommand;
 use Terpz710\Homes\Command\HomesCommand;
-use Terpz710\Homes\Command\SetHomeCommand;
 use Terpz710\Homes\Command\DelHomeCommand;
 
 class Main extends PluginBase {
 
-    /** @var HomeManager */
-    private $homeManager;
-
     public function onEnable(): void {
-        $this->homeManager = new HomeManager($this);
-        $this->getServer()->getCommandMap()->register("sethome", new SetHomeCommand($this->homeManager));
-        $this->getServer()->getCommandMap()->register("home", new HomeCommand($this->homeManager));
-        $this->getServer()->getCommandMap()->register("delhome", new DelHomeCommand($this->homeManager));
-        $this->getServer()->getCommandMap()->register("homes", new HomesCommand($this->homeManager));
+
+        $this->createHomesDirectory();
+
+        $this->getServer()->getCommandMap()->register("sethome", new SetHomeCommand($this->getDataFolder()));
+        $this->getServer()->getCommandMap()->register("home", new HomeCommand($this->getDataFolder()));
+        $this->getServer()->getCommandMap()->register("delhome", new DelHomeCommand($this->getDataFolder()));
+        $this->getServer()->getCommandMap()->register("homes", new HomesCommand($this->getDataFolder()));
+    }
+
+    private function createHomesDirectory(): void {
+        $dataFolder = $this->getDataFolder();
+        if (!is_dir($dataFolder . "homes")) {
+            mkdir($dataFolder . "homes", 0755, true);
+        }
     }
 }
