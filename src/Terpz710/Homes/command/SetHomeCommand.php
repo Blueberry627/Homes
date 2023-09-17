328f6d9f954d55e94a510e60a2d3b0411a6063a6
Blueberry627 <145240944+Blueberry627@users.noreply.github.com>
2023-09-17 03:35:45 -0700

    Update SetHomeCommand.php

--- src/Terpz710/Homes/command/SetHomeCommand.php
+++ src/Terpz710/Homes/command/SetHomeCommand.php
@@ -7,37 +7,57 @@ namespace Terpz710\Homes\Command;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-use Terpz710\Homes;
+use pocketmine\utils\Config;
+use pocketmine\math\Vector3;
+use Terpz710\Homes\Main;
 
 class SetHomeCommand extends Command {
 
-    public function __construct(HomeManager $homeManager) {
+    private $dataFolder;
+
+    public function __construct(string $dataFolder) {
         parent::__construct("sethome", "Set your home location");
         $this->setPermission("homes.sethome");
+        $this->dataFolder = $dataFolder;
     }
 
     public function execute(CommandSender $sender, string $label, array $args): bool {
         if ($sender instanceof Player) {
-            $player = $sender;
-
             if (!$this->testPermission($sender)) {
                 $sender->sendMessage("You do not have permission to use this command.");
                 return true;
             }
 
+            if (empty($args)) {
+                $sender->sendMessage("Usage: /sethome <home>");
+                return false;
+            }
+
+            $homeName = $args[0];
             $homeLocation = [
-                'x' => $player->getX(),
-                'y' => $player->getY(),
-                'z' => $player->getZ(),
-                'world' => $player->getLevel()->getName(),
+                'x' => $sender->getX(),
+                'y' => $sender->getY(),
+                'z' => $sender->getZ(),
+                'world' => $sender->getLevel()->getName(),
             ];
 
-            $this->homeManager->setHome($player, $homeLocation, "home");
+            $this->saveHomeData($sender, $homeName, $homeLocation);
 
-            $sender->sendMessage("Your home location 'home' has been set!");
+            $sender->sendMessage("Your home location '$homeName' has been set!");
         } else {
             $sender->sendMessage("This command can only be used in-game.");
         }
         return true;
+    }
+
+    private function saveHomeData(Player $player, string $homeName, array $homeLocation): void {
+        $playerName = strtolower($player->getName());
+        $config = new Config($this->dataFolder . "homes/" . $playerName . ".yml", Config::YAML);
+
+        $playerHomes = $config->get("homes", []);
+        $playerHomes[$homeName] = $homeLocation;
+
+        $config->set("homes", $playerHomes);
+        $config->save();
     }
 }
